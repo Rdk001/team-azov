61fc32653b432069facb579663382bb53839976d
Rdk001 <rmaslenchenko95@gmail.com>
2023-01-31 10:04:11 +0100

    поменяли скрипты

--- src/js/mobile_menu.js
+++ src/js/mobile_menu.js
@@ -1,14 +1,20 @@
 (() => {
-  const menuBtnRef = document.querySelector('[data-menu-button]');
-  const mobileMenuRef = document.querySelector('[data-menu]');
-
-  menuBtnRef.addEventListener('click', () => {
-    const expanded =
-      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-
-    menuBtnRef.classList.toggle('is-open');
-    menuBtnRef.setAttribute('aria-expanded', !expanded);
-
-    mobileMenuRef.classList.toggle('is-open');
-  });
+  const refs = {
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+    body: document.querySelector('body'),
+    menuList: document.querySelector('.mob-menu__list'),
+  };
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.menuList.addEventListener('click', removeMenu);
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+  function removeMenu() {
+    refs.menu.classList.add('is-hidden');
+    refs.body.classList.remove('no-scroll');
+  }
 })();
