c51ca75065a12c8c7dac8721ac67d78b4bcbdebe
Rdk001 <rmaslenchenko95@gmail.com>
2023-02-01 08:24:55 +0000

    Deploying to gh-pages from @ Rdk001/team-azov@782abb896f416edfdf9eaa02b24d84dd4c87a250 🚀

---
+++ index.190c5bd7.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".mySwiper",{loop:!0,autoplay:{delay:2e4},keyboard:{enabled:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.190c5bd7.js.map
